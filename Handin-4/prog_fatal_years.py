448d4ca62fc83f802512a4caecbf968a153900af
Lasse Hansen <Lasse Hansen>
2017-03-29 09:27:01 +0200

    Fixed to use proper column to determine year

--- Handin-4/prog_fatal_years.py
+++ Handin-4/prog_fatal_years.py
@@ -7,15 +7,13 @@ years_dict = defaultdict(lambda: 0)
 aviation_data = pd.read_csv("AviationDataset.csv", encoding='ISO-8859-2')
 
 for row in aviation_data.itertuples():
-	tmp = row[1][0:4]
-	
-	if(math.isnan(row[24]) == False):
-		years_dict[tmp] += row[24]
+    tmp = row[4][0:4]
+    
+    
+    if(math.isnan(row[24]) == False and tmp > '1997-01-01'):
+        years_dict[tmp] += row[24]
 
 plt.bar(range(len(years_dict)), years_dict.values(), align='center')
 plt.xticks(range(len(years_dict)), list(years_dict.keys()))
 
 plt.show()
-
-# column1 = 20170103X43747 (YYYYmmDD)
-# column24 = total fatal injuries
