a09a345e1276b1b362f6d4890b601fe71ab2dfd1
Patrick Johansen <Patrick Johansen>
2017-05-19 16:12:29 +0200

    Lots of clean up and implementation of plotting and a result dict

--- exam/code/prog_main.py
+++ exam/code/prog_main.py
@@ -6,6 +6,9 @@ import sys
 from prog_isolate import read, create_mask, create_mask_rgb, find_contours
 from prog_compare import compare
 import os
+import pprint
+import operator
+from prog_plot import plot_results_bar, prepare_for_plotting
 
 def printimage(img):
 	while True:
@@ -21,11 +24,13 @@ def get_crop(item, image):
 	crop_tmp = image[y:y+h, x:x+w]
 	crop_tmp50x50 = cv2.resize(crop_tmp, (50,50), interpolation=cv2.INTER_AREA)
 	return crop_tmp50x50
-
-def compare_against(source_crop, platform):
+	
+def compare_against_v2(source_crop, platform):
 	comparables = [f for f in os.listdir(platform) if os.path.isfile(os.path.join(platform, f))]
 	
 	res = []
+	
+	boundary = 10000
 
 	smallest_name = "unknown"
 	smallest_value = 99999
@@ -37,8 +42,7 @@ def compare_against(source_crop, platform):
 		)
 	)
 	
-	if(__test_diff__ > 10000):
-		print("probably shit")
+	if(__test_diff__ > boundary):
 		return (smallest_name, smallest_value)
 
 	for item in comparables:	
@@ -48,47 +52,54 @@ def compare_against(source_crop, platform):
 				platform + "/" + item
 			)
 		)
-		#print("diff is " + str(diff))
 		if(diff < smallest_value):
 			smallest_value = diff
 			smallest_name = item
 
 	return (smallest_name, smallest_value)
-	
+
+def compare_against(source_crop, platform):
+	comparables = [f for f in os.listdir(platform) if os.path.isfile(os.path.join(platform, f))]
+	res = []
+	smallest_name = "unknown"
+	smallest_value = 99999
+
+	for item in comparables:	
+		diff = compare(
+			source_crop,
+			cv2.imread(
+				platform + "/" + item
+			)
+		)
+		if(diff < smallest_value):
+			smallest_value = diff
+			smallest_name = item
+
+	return (smallest_name, smallest_value)
 
 def try_detect(image_file, lower, upper, path, masktype, erode=0, dilate=0):
 	platform_mask = masktype(image_file, lower, upper, erode, dilate)
 	platform_contours = find_contours(platform_mask, 50) # prev was 1
 	platform_best_match = ("Unknown", 99999)
 	
-	idx = 0
-	
+	#idx = 0
 	verygoodfit = 1000
 	
 	for item in platform_contours:
-		idx += 1
+		#idx += 1
 		
-		print(str(idx) + "/" + str(len(platform_contours)))
+		#print(str(idx) + "/" + str(len(platform_contours)))
 		my_crop = get_crop(item, image_file)
-		tmpres = compare_against(my_crop, path )
-		#print(tmpres)
+		tmpres = compare_against_v2(my_crop, path )
+
 		if(tmpres[1] < platform_best_match[1]):
 			platform_best_match = (tmpres[0], tmpres[1])
 		if tmpres[1] < verygoodfit:
-			print("Broke because very good fit")
+			print("Found a good fit, moving on to next.")
 			break
 	
 	
 	return platform_best_match
-
-def print_best_fit(data, platform):
-	if(data[1] <= 1000):
-		print(platform)
-		print(data)
-		exit()
-	else:
-		print("Checked for " + platform)
-		print(data)
 
 if __name__ == "__main__":
 	args = sys.argv
@@ -100,5 +111,13 @@ if __name__ == "__main__":
 	
 	image_file = cv2.imread(args[1])
 	
-	print_best_fit(try_detect(image_file, (20,50,50), (100,255,255), "./realxbox", create_mask, 2, 2), "xbox")
-	print_best_fit(try_detect(image_file, (200,200,200), (255,255,255), "./realps", create_mask_rgb, 0, 2), "ps4")
+	pp = pprint.PrettyPrinter(indent=4)
+	
+	results = {}
+	print("Calculating...")
+	results["Xbox 360"] = try_detect(image_file, (20,50,50), (100,255,255), "./realxbox", create_mask, 2, 2)[1]
+	results["PS4"] = try_detect(image_file, (200,200,200), (255,255,255), "./realps", create_mask_rgb, 0, 2)[1]
+	results["Nintendo GCN"] = try_detect( image_file, (45,45,100), (211,211,224), "./realgc", create_mask_rgb, 0, 2 )[1]
+	results["Nintendo DS"] = try_detect( image_file, (0,0,0), (40,40,40), "./realds", create_mask_rgb, 0, 3 )[1]
+	results["Nintendo Wii U"] = try_detect( image_file, (180,180,180), (255,255,255), "./realwiiu", create_mask_rgb)[1]
+	plot_results_bar( prepare_for_plotting(results) )
